a5ccff7d9ec5bfd2bfedda8f4ac45b045ca4c533
Sushmitha Prathap <prathap.s@northeastern.edu>
2023-10-04 20:11:16 -0400

    db changed

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize("assignment3", "root", "1998@Pupss", {
   host: "localhost",
-  dialect: "mariadb",
+  dialect: "mysql",
 });
 
 export default sequelize;
