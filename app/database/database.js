e63b362f2a2a987ec5627f63bb5d26642b46e042
Sushmitha Prathap <113482457+SushmithaPrathap@users.noreply.github.com>
2023-10-25 16:33:41 -0400

    db change host (#48)

--- app/database/database.js
+++ app/database/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize("assignment3", "root", "1998@Pupss", {
-  host: "localhost",
+  host: "127.0.0.1",
   dialect: "mysql",
   logging: false
 });
