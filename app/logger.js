e50a77d2a1123fcb4bf4b1ac28f1964fe23362e6
Sushmitha Prathap <prathap.s@northeastern.edu>
2023-11-08 14:17:57 -0500

    winston setup

--- app/logger.js
+++ app/logger.js
@@ -1,4 +1,16 @@
-import pino from "pino";
+import winston from "winston";
 
-const logger = pino({});
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(), // Add timestamp to logs
+    winston.format.json()
+  ),
+  defaultMeta: { service: "webapp" },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "webapp.log" }),
+  ],
+});
+
 export default logger;
